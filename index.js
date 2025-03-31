7efd92ff18c0b3149cd864aa0414e7375e3b4da4
Cristian Berrio <cristianberrio9864@gmail.com>
2025-03-31 11:35:24 -0500

    Up

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const functions = require("firebase-functions");
+// 🔹 Importaciones
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
 
+// 🔹 Inicialización de la app y el servidor
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "../public")));
+// 🔹 Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
+// 🔹 Rutas
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// 🔹 Manejo de WebSockets
 io.on("connection", (socket) => {
-    console.log("Un usuario se ha conectado");
+    console.log("🔵 Un usuario se ha conectado");
 
     socket.on("join", (name) => {
-        socket.broadcast.emit("message", { name: "Sistema", message: `${name} se ha unido al chat`, time: new Date().toLocaleTimeString() });
+        socket.broadcast.emit("message", {
+            name: "Sistema",
+            message: `${name} se ha unido al chat`,
+            time: new Date().toLocaleTimeString(),
+        });
     });
 
     socket.on("message", (msg) => {
@@ -26,8 +34,12 @@ io.on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
-        console.log("Un usuario se ha desconectado");
+        console.log("🔴 Un usuario se ha desconectado");
     });
 });
 
-exports.app = functions.https.onRequest(app);
+// 🔹 Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
