ae41d6a587ff1a6d41370b1aff61d3ef113862e6
Cristian Berrio <cristianberrio9864@gmail.com>
2025-03-31 11:14:33 -0500

    all

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado');
+
+    socket.on('join', (name) => {
+        socket.broadcast.emit('message', { name: 'Sistema', message: `${name} se ha unido al chat`, time: new Date().toLocaleTimeString() });
+    });
+
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un usuario se ha desconectado');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+});
