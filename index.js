e27518ccb1f227fce57cdc4760bc6b41f282921c
Cristian Berrio <cristianberrio9864@gmail.com>
2025-03-31 15:26:06 -0500

    Notificaciones

--- index.js
+++ index.js
@@ -20,30 +20,42 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/notifications", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notifications.html"));
+});
+
 app.post("/webhook", (req, res) => {
     const webhookData = req.body;
-  
+
     // Convertir el objeto a un string JSON con formato legible
     const dataToWrite = JSON.stringify(webhookData, null, 2);
-  
+
     // Escribir el contenido en el archivo log.json
     fs.writeFile("log.json", dataToWrite, (err) => {
-      if (err) {
-        console.error("Error al escribir en log.json:", err);
-        return res.status(500).send("Error al guardar los datos del webhook.");
-      }
-      console.log("Datos del webhook guardados correctamente.");
-      res.status(200).send("Webhook recibido y datos guardados.");
+        if (err) {
+            console.error("Error al escribir en log.json:", err);
+            return res.status(500).send("Error al guardar los datos del webhook.");
+        }
+        console.log("Datos del webhook guardados correctamente.");
+
+        var msg = webhookData.data.buyer.name + " " + webhookData.data.buyer.last_name + " ha sido despertado de la Matrix"
+
+        // Emitir una notificación a todos los clientes conectados
+        io.emit("webhookNotification", {
+            msg
+        });
+
+        res.status(200).send("Webhook recibido, datos guardados y notificación enviada.");
     });
 });
 
 app.get("/descargar-log", (req, res) => {
     const file = `${__dirname}/log.json`;
     res.download(file, "log.json", (err) => {
-      if (err) {
-        console.error("Error al enviar el archivo:", err);
-        res.status(500).send("Error al enviar el archivo.");
-      }
+        if (err) {
+            console.error("Error al enviar el archivo:", err);
+            res.status(500).send("Error al enviar el archivo.");
+        }
     });
 });
 
